921b483a6a938c0c80fe9ecb9879a82fc7965955
buguzei <acmkantemir@gmail.com>
2023-09-09 23:34:03 +0300

    Merge Sorted Array commit

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	nums := []int{1, 2, 0, 3}
-	fmt.Println(solving.DuplicateZeros(nums))
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	fmt.Println(solving.MergeSortedArray(nums1, nums2, 3, 3))
 }
---
+++ solving/MergeSortedArray.go
@@ -0,0 +1,18 @@
+package solving
+
+func MergeSortedArray(nums1, nums2 []int, m, n int) []int {
+	mergeNums1 := nums1[:m]
+	mergeNums2 := nums2[:n]
+
+	mergeNums := append(mergeNums1, mergeNums2...)
+
+	for i := 0; i < len(mergeNums)-1; i++ {
+		for j := 0; j < len(mergeNums)-i-1; j++ {
+			if mergeNums[j+1] < mergeNums[j] {
+				mergeNums[j], mergeNums[j+1] = mergeNums[j+1], mergeNums[j]
+			}
+		}
+	}
+
+	return mergeNums
+}
